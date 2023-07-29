36615f39bc51d0e1984e293ede9dda2fea46fb8c
Claudia Yepes <claudia.yepes0806@hotmail.com>
2023-07-29 13:52:15 -0500

    primer commit

---
+++ Jikan.WebApi/Domain/GetSeriesByDescriptionInteractor.cs
@@ -0,0 +1,44 @@
+﻿using Jikan.WebApi.Entities;
+using System.Text.Json;
+
+namespace Jikan.WebApi.Domain
+{
+    public class GetSeriesByDescriptionInteractor : IGetSeriesByDescriptionInteractor
+    {
+        readonly IJikanService JikanService;
+
+        public GetSeriesByDescriptionInteractor(IJikanService jikanService)
+        {
+            JikanService = jikanService;
+        }
+
+        public async Task<JikanResponse> GetSeriesByDescription(string description, int page)
+        {
+
+            // Consultar el api de Jikan
+            string jsonResponse = await JikanService.GetSeriesByDescription(description, page);
+
+            // Deserializar respuesta
+            JikanResponse response = JsonSerializer.Deserialize<JikanResponse>(jsonResponse);
+
+            // Hacer los cálculos
+            foreach (var item in response.data)
+            {
+                if (item.score >= 1 & item.score <= 4)
+                {
+                    item.message = "I do not recommend it";
+                }
+                if (item.score >= 5 & item.score <= 7)
+                {
+                    item.message = "You may have fun";
+                }
+                if (item.score > 7)
+                {
+                    item.message = "Great, this is one of the best anime";
+                }
+           }
+
+            return response;
+        }
+    }
+}
---
+++ Jikan.WebApi/Entities/Data.cs
@@ -0,0 +1,12 @@
+﻿namespace Jikan.WebApi.Entities
+{
+    public class Data
+    {
+        public int mal_id { get; set; }
+        public string title { get; set; }
+        public double? score { get; set; }
+        public Image images { get; set; }
+        public string message { get; set; }
+
+    }
+}
